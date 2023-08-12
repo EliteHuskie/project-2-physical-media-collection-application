2ed9ba6a87160f8ed12f6b2c6328120e498786bb
Sarah S <stms15@mun.ca>
2023-08-12 20:34:25 -0230

    added user sign up api call

--- public/js/login-script.js
+++ public/js/login-script.js
@@ -1,4 +1,4 @@
-// Listener for signup/login
+// Listener for login
 const usernameEl = document.getElementById("username");
 const passwordEl = document.getElementById("password");
 const loginBttnEl = document.getElementById("login");
@@ -8,6 +8,30 @@ loginBttnEl.addEventListener("click", async (event) => {
   const passwordInput = passwordEl.value;
 
   const response = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: usernameInput, password: passwordInput }),
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    window.location.href = "/home";
+  } else {
+    alert(`Error: ${response.status}\n${data.message}`);
+  }
+});
+
+// Listener for signup
+const registerBttnEl = document.getElementById("register");
+
+registerBttnEl.addEventListener("click", async (event) => {
+  const usernameInput = usernameEl.value;
+  const passwordInput = passwordEl.value;
+
+  const response = await fetch("/api/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
